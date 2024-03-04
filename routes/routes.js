5298bc343e0cb44dc391fa8cf46a246fd437228b
PerlaGCastillo <contactoperlagodinez@gmail.com>
2024-03-04 16:59:16 -0600

    testing w image uploader in progress

--- routes/routes.js
+++ routes/routes.js
@@ -17,6 +17,27 @@ var upload = multer({
     storage: storage,
 }).single('image')
 
+//isnert an user into db
+router.post('/add', upload, (req, res)=>{
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        image: req.file.filename,
+    })
+    user.save().then(()=>{
+        req.session.message = {
+            type: 'success',
+            message: 'user added successfuly'
+        }
+        res.redirect('/')
+    }).catch((err)=>{
+        res.json({message: err.message, type: 'danger'})
+    })
+
+})
+        
+
 router.get('/', (req, res)=> {
     res.render('index', {title: 'Home Page'})
 })
