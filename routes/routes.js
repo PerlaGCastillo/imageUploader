36df7570060eaf0ecf634c3b43ecbf48597b1164
PerlaGCastillo <contactoperlagodinez@gmail.com>
2024-03-04 19:36:33 -0600

    error carga

--- routes/routes.js
+++ routes/routes.js
@@ -1,49 +1,60 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/users')
-const multer = require('multer')
+const express = require("express");
+const router = express.Router();
+const User = require("../models/users");
+const multer = require("multer");
 
 //image upload
 var storage = multer.diskStorage({
-    destination: function(req, file, cb){
-        cb(null, './uploads')
-    }, 
-    filename: function(req, file, cb){
-        cb(null, file.fieldname+"_"+Date.now()+"_"+file.originalname)
-    },
-})
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+  },
+});
 
 var upload = multer({
-    storage: storage,
-}).single('image')
+  storage: storage,
+}).single("image");
 
 //isnert an user into db
-router.post('/add', upload, (req, res)=>{
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        phone: req.body.phone,
-        image: req.file.filename,
+router.post("/add", upload, (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    image: req.file.filename,
+  });
+  user
+    .save()
+    .then(() => {
+      req.session.message = {
+        type: "success",
+        message: "user added successfuly",
+      };
+      res.redirect("/");
     })
-    user.save().then(()=>{
-        req.session.message = {
-            type: 'success',
-            message: 'user added successfuly'
-        }
-        res.redirect('/')
-    }).catch((err)=>{
-        res.json({message: err.message, type: 'danger'})
+    .catch((err) => {
+      res.json({ message: err.message, type: "danger" });
+    });
+});
+
+//get all users route
+router.get("/", (req, res) => {
+  User.find()
+    .then((err, users) => {
+      res.render("index", {
+        title: "home",
+        users: users,
+      });
     })
+    .catch((err) => {
+      res.json({ message: err.message });
+    });
+});
 
-})
-        
+router.get("/add", (req, res) => {
+  res.render("add_user", { title: "Add Users" });
+});
 
-router.get('/', (req, res)=> {
-    res.render('index', {title: 'Home Page'})
-})
-
-router.get('/add', (req, res)=> {
-    res.render('add_user', {title: 'Add Users'})
-})
-
-module.exports = router
+module.exports = router;
